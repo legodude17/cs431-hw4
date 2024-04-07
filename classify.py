9afd69f3a2eb40c65eaef19cf5610240e3b8db1d
JDB <legodude17@users.noreply.github.com>
2024-04-07 11:59:37 -0700

    Add comments

--- classify.py
+++ classify.py
@@ -1,3 +1,4 @@
+# Imports
 import os
 import sys
 os.environ["KERAS_BACKEND"] = "tensorflow"
@@ -5,26 +6,30 @@ os.environ["KERAS_BACKEND"] = "tensorflow"
 import tensorflow as tf
 import keras
 
+# Constants
 FILE = sys.argv[1]
 IMGS = sys.argv[2:]
 
-model = keras.models.load_model(FILE)
+model = keras.models.load_model(FILE) # Load the model
 
-dataset = tf.data.Dataset.from_tensor_slices(IMGS).map(
-  lambda x: tf.io.decode_image(tf.io.read_file(x), channels=3, expand_animations=False)
+# Loading the data
+
+dataset = tf.data.Dataset.from_tensor_slices(IMGS).map( # Load the given images
+  lambda x: tf.io.decode_image(tf.io.read_file(x), channels=3, expand_animations=False), num_parallel_calls=tf.data.AUTOTUNE # Load the images
 ).map(
-  lambda x: keras.layers.Rescaling(1./255)(x)
-).batch(16)
+  lambda x: keras.layers.Rescaling(1./255)(x) # Rescale the data, like the trainer does
+).batch(16).prefetch(tf.data.AUTOTUNE) # Batch and prefetch the data
 
-predictions = model.predict(dataset)
+predictions = model.predict(dataset) # Make the predictions
 
+# Go through each prediction and print it
 for img, pred in zip(IMGS, predictions):
-  pred = float(keras.ops.sigmoid(pred[0]))
-  predict = "Unknown"
+  pred = float(keras.ops.sigmoid(pred[0])) # Result of the sigmoid is a probability
+  # Closer to 1 is a dog, closer to 0 is a cat, since that's the labels I chose
   if pred > 0.5:
     predict = "Dog"
   elif pred < 0.5:
     predict = "Cat"
 
-  print(img + ": " + predict)
+  print(img + ": " + predict) # Print the prediction
 
--- make_nn.py
+++ make_nn.py
@@ -1,3 +1,4 @@
+# Imports
 import os
 import sys
 os.environ["KERAS_BACKEND"] = "tensorflow"
@@ -6,19 +7,23 @@ import tensorflow as tf
 import keras
 from keras import layers
 
-EPOCHS = 25
+# Constants
+EPOCHS = 25 # 25 epochs takes about 17 hours on my laptop, but gives around 95% accuracy
 DIR = sys.argv[1]
 FILE = sys.argv[2]
 
+# Data Loading
+
+# Data augmentation layers
 augment_layers = [
   layers.RandomFlip("horizontal"),
   layers.RandomRotation(0.1),
   layers.Rescaling(1./255)
 ]
 
+# Process a path, by loading the image and getting the label
 @tf.function
 def process_path(x):
-  file = tf.io.read_file(x)
   label = tf.strings.split(x, os.sep)[-1]
   label = tf.strings.substr(label, 0, 1)
   if tf.math.equal(label, "c"):
@@ -27,31 +32,37 @@ def process_path(x):
     label = 1.
   else:
     label = -1.
+  file = tf.io.read_file(x)
   image = tf.io.decode_image(file, channels=3, expand_animations=False)
   return image, label
 
+# Augment the data using the augmentation layers
 def augment_data(images):
   for layer in augment_layers:
     images = layer(images, training=True)
   return images
 
-dataset = tf.data.Dataset.list_files(DIR + "/*.jpg")
-dataset = dataset.shuffle(dataset.cardinality()).map(
-  process_path, num_parallel_calls=tf.data.AUTOTUNE
+dataset = tf.data.Dataset.list_files(DIR + "/*.jpg") # Get all jpgs in the trainging folder
+dataset = dataset.shuffle(dataset.cardinality()).map( # Shuffle them fully
+  process_path, num_parallel_calls=tf.data.AUTOTUNE # Process all the paths
 )
 dataset = dataset.map(
-  lambda x, y: (augment_data(x), y), num_parallel_calls=tf.data.AUTOTUNE
+  lambda x, y: (augment_data(x), y), num_parallel_calls=tf.data.AUTOTUNE # Augment all the data
 )
-dataset = dataset.batch(128).prefetch(tf.data.AUTOTUNE)
+dataset = dataset.batch(128).prefetch(tf.data.AUTOTUNE) # Batch and prefetch the data, to increase training performance
 
-inputs = keras.Input((100, 100, 3))
+# Building the Model
 
+inputs = keras.Input((100, 100, 3)) # Inputs
+
+# First convolution
 x = layers.Conv2D(128, 3, strides=2, padding="same")(inputs)
 x = layers.BatchNormalization()(x)
 x = layers.Activation("relu")(x)
 
-prev_act = x
 
+# Create the residual layers
+prev_act = x
 for size in [256, 512, 728]:
   x = layers.Activation("relu")(x)
   x = layers.SeparableConv2D(size, 3, padding="same")(x)
@@ -67,26 +78,31 @@ for size in [256, 512, 728]:
   x = layers.add([x, residual])
   prev_act = x
 
+# Final convolution
 x = layers.SeparableConv2D(1024, 3, padding="same")(x)
 x = layers.BatchNormalization()(x)
 x = layers.Activation("relu")(x)
 
+# Output
 x = layers.GlobalAveragePooling2D()(x)
-
 x = layers.Dropout(0.25)(x)
+outputs = layers.Dense(1, activation=None)(x) # Outputs
 
-outputs = layers.Dense(1, activation=None)(x)
-
+# Create the model
 model = keras.Model(inputs, outputs)
 
+# Compile the model, we're using binary mode, since we have two classes
 model.compile(
   optimizer=keras.optimizers.Adam(3e-4),
   loss=keras.losses.BinaryCrossentropy(from_logits=True),
   metrics=[keras.metrics.BinaryAccuracy(name="acc")],
 )
 
-model.summary()
+# Print a summary of the model
+#model.summary()
 
+# Fit the model to the dataset
 model.fit(dataset, epochs=EPOCHS)
 
+# Save the model
 model.save(FILE, include_optimizer = False)
